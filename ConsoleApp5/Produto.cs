cc396ca3e5bd6c60da4a11a94ed67f85a059254e
Guilherme Ramos <105815487+GuilhermeRamosAju@users.noreply.github.com>
2024-08-01 22:28:41 -0300

    Criação de classes e construtores

---
+++ ConsoleApp5/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp5
+{   // Declaração de Classe
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        // Criação de construtor para ser obrigatória a inserção dos atributos ao instanciar o objeto
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p =  new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            if (int.TryParse(Console.ReadLine(), out int qteAdicionar))
+            {
+                p.AdicionarProdutos(qteAdicionar);
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            if (int.TryParse(Console.ReadLine(), out int qteRemover))
+            {
+                p.RemoverProdutos(qteRemover);
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
